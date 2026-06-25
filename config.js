8192caf8ae4ce94a5101633b98353bc727376880
Davinto4 <201003314+Davinto4@users.noreply.github.com>
2026-06-25 16:26:43 +0100

    Add configuration file for bot settings

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    botName: 'DavintoMD',
+    ownerName: 'Davinto',
+    ownerNumber: '2348169045105',
+    prefix: '.',
+    channelLink: 'https://whatsapp.com/channel/0029Vb1V3aWC6ZvguZLoMv40',
+    menuImage: 'https://i.supaimg.com/f31a85b4-8ed0-43c6-84c3-df2e2387d7ea/ad3cf6cb-bed1-4414-b342-1648e634b35a.png',
+    websiteUrl: 'https://davintomd.netlify.app/',
+    supportContact: '2348169045105',
+    version: '1.0.0',
+    mode: 'public', // 'public' or 'private'
+    dbType: 'json'  // 'json', 'mongo', 'sqlite'
+};
